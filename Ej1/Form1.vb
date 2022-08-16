1d653c6d49da2a9e41f395ba21fa805631ec607f
Matías Sio <94750147+matiassio2774@users.noreply.github.com>
2022-08-16 08:13:03 -0300

    Add files via upload

---
+++ Ej1/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Show()
+
+        Dim parar As Integer
+
+        Do While parar < 1000000
+
+            Me.Label1.Text = parar.ToString()
+
+            parar = parar + 1
+
+        Loop
+        Form2.Show()
+    End Sub
+End Class
